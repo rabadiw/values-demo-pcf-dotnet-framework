1bd76d6c519631cd34a6512c21eab7075710f813
Wael Rabadi <wrabadi@pivotal.io>
2018-11-15 10:30:48 -0600

    Add project files.

---
+++ demo.values/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace demo.values
+{
+  public class FilterConfig
+  {
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+    {
+      filters.Add(new HandleErrorAttribute());
+    }
+  }
+}
